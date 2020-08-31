a3bb18f932edcfa3ecca770d0026f59d58c558ab
Sergey Rovkach <srovk22@gmail.com>
2020-08-31 16:07:01 +0300

    #17. refactor background function

--- js/plugins/header/backgrounds.js
+++ js/plugins/header/backgrounds.js
@@ -5,13 +5,5 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-import background1 from '../../../assets/img/backgrounds/background1.jpg';
-import background2 from '../../../assets/img/backgrounds/background2.jpg';
-import background3 from '../../../assets/img/backgrounds/background3.jpg';
-
-export const backgroundImgs = [
-    background1,
-    background2,
-    background3
-];
+const importAll = (r) => r.keys().map(r);
+export const backgroundImgs = importAll(require.context('../../../assets/img/backgrounds/', false, /\.(png|jpe?g|svg)$/));
